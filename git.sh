5cdb20505c9d61a924aeab859a1184ac8891431b
freelstephen2026 <freelstephen3@gmail.com>
2023-02-10 20:39:45 -0500

    New commit

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@ git pull
 git init
 git add .
 git commit -m "New commit"
-git remote add origin https://github.com/freelstephen2026/fix-my-bug.git
-git push origin new-master
+git remote add origin https://github.com/freelstephen2026/OOG.git
+git push origin master
